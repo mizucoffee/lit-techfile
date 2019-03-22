4ec748df5e19ecff595bc88a18059656f7457623
Ritsuki Goto <develop@mizucoffee.net>
2019-03-22 14:08:46 +0900

    [add] quiz

--- quiz/app.rb
+++ quiz/app.rb
@@ -4,7 +4,45 @@ require 'sinatra/reloader' if development?
 require './models.rb'
 
 before do
-    if Result.all.length == 0 
+    if Result.all.length == 0
         Result.create(score:0)
     end
+
+    @questions = Array.new
+    @questions.push(["Webサービスプログラミングコースで学んでいる、赤色の宝石の名前を使っている言語は？", "Garnet", "Ruby", "Amethyst", "Opal", "2"])
+    @questions.push(["Life is Tech!のキャンプのコースはいくつあるでしょう？", "14", "15", "16", "17", "4"])
+end
+
+get '/' do
+    result = Result.first
+    result.score = 0
+    result.save
+    redirect '/question/0'
+end
+
+get '/question/:id' do
+    @number = params[:id].to_i
+    erb :question
+end
+
+post '/question/check/:id' do
+    number = params[:id].to_i
+    answer = @questions[number][5]
+    select_answer = params[:select_answer]
+    if answer == select_answer
+        result = Result.first
+        result.score += 1
+        result.save
+    end
+    if number + 1 < @questions.length
+        number += 1
+        redirect "/question/#{number}"
+    else
+        redirect "/result"
+    end
+end
+
+get '/result' do
+    @score = Result.first.score
+    erb :result
 end
