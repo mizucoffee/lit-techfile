878a64bb5da91b7f3d324206b19a503a30ae7720
Ritsuki Goto <develop@mizucoffee.net>
2019-10-30 00:51:31 +0900

    [add] Album

---
+++ Android/M04.Album/app/src/main/java/net/mizucoffee/album/PreviewActivity.kt
@@ -0,0 +1,21 @@
+package net.mizucoffee.album
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.*
+
+class PreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+
+        val imageId: Int = intent.getIntExtra("image", 0)
+
+        previewImage.setImageResource(imageId)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
