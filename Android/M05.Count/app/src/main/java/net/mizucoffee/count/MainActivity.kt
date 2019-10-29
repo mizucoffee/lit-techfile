f7a1c1211d7da12d82e3b638242ae1f80ab2655e
Ritsuki Goto <develop@mizucoffee.net>
2019-10-30 01:07:00 +0900

    [add] Count

---
+++ Android/M05.Count/app/src/main/java/net/mizucoffee/count/MainActivity.kt
@@ -0,0 +1,25 @@
+package net.mizucoffee.count
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var number: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        count_text_view.text = "0"
+        plus_button.setOnClickListener {
+            number = number + 1
+            count_text_view.text = number.toString()
+        }
+        minus_button.setOnClickListener {
+            number = number - 1
+            count_text_view.text = number.toString()
+        }
+    }
+}
